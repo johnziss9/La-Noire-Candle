80ea8e44fc9b286575d6910c422fe42ada451c4e
John Z <john.zissimou@and.digital>
2022-11-30 23:41:23 +0000

    Created components for LogoHeader and Navbar. Installed Boostrap. Added a center aligned navbar. Called the two new components in Home.

---
+++ src/components/LogoHeader/LogoHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LogoHeader.css';
+
+function LogoHeader() {
+
+  return (
+    <>
+      <div className='logo-header-container'>This is the logo header</div>
+    </>
+  );
+}
+
+export default LogoHeader;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import LogoHeader from '../../components/LogoHeader/LogoHeader';
+import Navbar from '../../components/Navbar/Navbar';
 
 function Home() {
 
   return (
     <>
-      THIS IS THE HOMEPAGE!
+      <LogoHeader />
+      <Navbar />
     </>
   );
 }
