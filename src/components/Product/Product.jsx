f563505dcbf016051fab8d3654b953293e6d7ff9
John Z <john.zissimou@and.digital>
2022-12-05 23:56:52 +0000

    Added a product component and fitted it into the home page. Made it responsive for mobile too.

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Product.css';
+
+function Product() {
+
+  return (
+    <div className='product-container'>
+      <div className='product-image-panel'>
+        IMAGE GOES HERE
+      </div>
+      <div className='product-text-panel'>
+        TEXT GOES HERE
+      </div>
+    </div>
+  );
+}
+
+export default Product;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import LogoHeader from '../../components/LogoHeader/LogoHeader';
 import Navbar from '../../components/Navbar/Navbar';
+import Product from '../../components/Product/Product';
 import "./Home.css";
 
 function Home() {
 
   return (
-    <>
+    <div className='home-container'>
       <div className="home-header">
         <LogoHeader />
         <Navbar />
       </div>
-      <hr />
-    </>
+      <div className="home-products-container">
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
   );
 }
 
