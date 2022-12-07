bf69d248348f2d64f250647d51240533f54053dd
John Z <john.zissimou@and.digital>
2022-12-07 23:41:22 +0000

    Created About component and added it to Home. Commented out all margins to make them consistent.

---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About(props) {
+    return (
+        <div className='about-container'>
+            <div className='about-title'>About Us</div>
+            ABOUT SECTION
+        </div>
+    );
+}
+
+export default About;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import LogoHeader from '../../components/LogoHeader/LogoHeader';
 import Navbar from '../../components/Navbar/Navbar';
 import Product from '../../components/Product/Product';
 import Banner from '../../components/Banner/Banner';
+import About from '../../components/About/About';
 import "./Home.css";
 import CandleImg from "../../images/candle.png";
 import AirFreshnerImg from "../../images/airfreshner.png";
@@ -18,10 +19,13 @@ function Home() {
       </div>
       <Banner src={BannerImg} alt="Banner Image" />
       <div className="home-products-container">
-        <div className='home-product-title'>Our Products</div>
+        <div className='home-products-title'>Our Products</div>
         <Product reverse={false} src={CandleImg} alt="Candle Image" text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
         <Product reverse={true} src={DefuserImg} alt="Defuser Image" text="t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
         <Product reverse={false} src={AirFreshnerImg} alt="Air Freshner Image" text="The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham." />
+      </div>
+      <div className="home-about-container">
+        <About />
       </div>
     </div>
   );
