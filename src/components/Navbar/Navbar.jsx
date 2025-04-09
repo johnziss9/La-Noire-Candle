ea523fb361565ef0687bef303a42c84e51b52fec
John Zissimou <john.zissimou@KT7T40F95F.local>
2025-04-09 17:38:55 +0100

    Changed the design of the contact us modal.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,8 +8,13 @@ import emailjs from "emailjs-com";
 
 function Navbar() {
     const [show, setShow] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
 
-    const handleClose = () => setShow(false);
+    const handleClose = () => {
+        setShow(false);
+        setTimeout(() => setSubmitted(false), 500); // Reset submission state after modal closes
+    };
+    
     const handleShow = () => setShow(true);
 
     const form = useRef();
@@ -17,16 +22,20 @@ function Navbar() {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        console.log(process.env.REACT_APP_PUBLIC_KEY);
-
-        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+        emailjs.sendForm(
+            process.env.REACT_APP_SERVICE_ID, 
+            process.env.REACT_APP_TEMPLATE_ID, 
+            form.current, 
+            process.env.REACT_APP_PUBLIC_KEY
+        )
+        .then((result) => {
+            console.log(result.text);
+            setSubmitted(true);
+            form.current.reset();
+        }, (error) => {
+            console.log(error.text);
+        });
     };
-
 
     return (
         <>
@@ -39,52 +48,65 @@ function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav mx-auto">
                         <li className="nav-item active">
-                            {/* eslint-disable-next-line */}
                             <Link className="nav-link" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            {/* eslint-disable-next-line */}
                             <Link className="nav-link" to="/CandleCare">Candle Care</Link>
                         </li>
                         <li className="nav-item">
-                            {/* eslint-disable-next-line */}
-                            <Link className="nav-link" href="#" onClick={handleShow}>Contact Us</Link>
+                            <Link className="nav-link" to="/About">About Us</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" onClick={handleShow}>Contact Us</Link>
                         </li>
                     </ul>
                 </div>
             </nav>
 
-            <Modal show={show} onHide={handleClose} backdrop='static'>
+            <Modal show={show} onHide={handleClose} backdrop='static' className="contact-modal">
                 <Modal.Header closeButton>
-                    <Modal.Title>Contact Us</Modal.Title>
+                    <Modal.Title>Get in Touch</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form ref={form} onSubmit={sendEmail}>
-                        <Form.Group className='contact-form-control-group'>
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control type="text" name='from_name' placeholder="Enter your name..." />
-                        </Form.Group>
-                        <Form.Group className='contact-form-control-group'>
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" name='from_email' placeholder="Enter your email..." />
-                        </Form.Group>
-                        <Form.Group className='contact-form-control-group'>
-                            <Form.Label>Contact Number</Form.Label>
-                            <Form.Control type="number" name='from_contact_number' placeholder="Enter your contact number..." />
-                        </Form.Group>
-                        <Form.Group className='contact-form-control-group'>
-                            <Form.Label>Message</Form.Label>
-                            <Form.Control as="textarea" name='from_message' rows="7" placeholder="Enter your message..." />
-                        </Form.Group>
-                        <Form.Group className='modal-button-group'>
-                            <Button variant="secondary" onClick={handleClose} className='modal-button'>
+                    {!submitted ? (
+                        <>
+                            <p className="modal-intro">We'd love to hear from you. Please fill out the form below, and we'll get back to you as soon as possible.</p>
+                            <Form ref={form} onSubmit={sendEmail}>
+                                <Form.Group className='contact-form-control-group'>
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control type="text" name='from_name' placeholder="Enter your name..." required />
+                                </Form.Group>
+                                <Form.Group className='contact-form-control-group'>
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" name='from_email' placeholder="Enter your email..." required />
+                                </Form.Group>
+                                <Form.Group className='contact-form-control-group'>
+                                    <Form.Label>Contact Number</Form.Label>
+                                    <Form.Control type="number" name='from_contact_number' placeholder="Enter your contact number..." />
+                                </Form.Group>
+                                <Form.Group className='contact-form-control-group'>
+                                    <Form.Label>Message</Form.Label>
+                                    <Form.Control as="textarea" name='from_message' rows="5" placeholder="Enter your message..." required />
+                                </Form.Group>
+                                <Form.Group className='modal-button-group'>
+                                    <Button variant="secondary" onClick={handleClose} className='modal-button'>
+                                        Cancel
+                                    </Button>
+                                    <Button variant="dark" type='submit' className='modal-button'>
+                                        Send Message
+                                    </Button>
+                                </Form.Group>
+                            </Form>
+                        </>
+                    ) : (
+                        <div className="submission-success">
+                            <h3>Thank You!</h3>
+                            <p>Your message has been sent successfully. We'll get back to you shortly.</p>
+                            <Button variant="dark" onClick={handleClose} className='modal-button-centered'>
                                 Close
                             </Button>
-                            <Button variant="dark" type='submit' className='modal-button'>
-                                Submit
-                            </Button>
-                        </Form.Group>
-                    </Form>
+                        </div>
+                    )}
                 </Modal.Body>
             </Modal>
         </>
